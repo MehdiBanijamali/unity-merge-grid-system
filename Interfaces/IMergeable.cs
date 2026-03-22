09cc05fc6344245af8df23f224accbfd45123923
MehdiBanijamali <103754068+MehdiBanijamali@users.noreply.github.com>
2026-03-22 12:06:14 +0330

    Update IMergeable.cs

--- Interfaces/IMergeable.cs
+++ Interfaces/IMergeable.cs
@@ -1,1 +1,9 @@
-
+namespace MergeGrid.Runtime.Interfaces
+{
+    public interface IMergeable
+    {
+        int Level { get; }
+        bool CanMerge(IMergeable other);
+        IMergeable Merge(IMergeable other);
+    }
+}
